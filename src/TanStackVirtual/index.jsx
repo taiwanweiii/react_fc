08d3d007b109c956d4bdce39e1f2dfeadc9b977b
ASIA\Willy_Chang <d410413@gmail.com>
2025-07-08 16:03:18 +0800

    TanStackVirtual

--- src/TanStackVirtual/index.jsx
+++ src/TanStackVirtual/index.jsx
@@ -3,13 +3,24 @@ import { Virtuoso } from "react-virtuoso";
     套件:Virtuoso
     
     用法
-        <div style={{ height: "400px" }}>
-            <TanStackVirtual
-                totalCount={9999}
-                data={data}
-                renderItem={renderItem}
-            />
-        </div>
+      const [data, setData] = useState([
+            { id: 0, post: "您好，第 1 則貼文" },
+            { id: 1, post: "您好，第 2 則貼文" },
+            { id: 2, post: "您好，第 3 則貼文" },
+            { id: 3, post: "您好，第 4 則貼文" },
+            { id: 4, post: "您好，第 5 則貼文" },
+            { id: 5, post: "您好，第 6 則貼文" },
+            { id: 6, post: "您好，第 7 則貼文" },
+        ]);
+        return(
+            <div style={{ height: "400px" }}>
+                <TanStackVirtual
+                    totalCount={9999}
+                    data={data}
+                    renderItem={renderItem}
+                />
+            </div>
+        )
 */
 export default ({
     data = null,
